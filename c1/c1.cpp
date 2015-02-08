9dd1ede29ae189635608db5775774292d792b9c5
brettalcox <brettalcox@gmail.com>
2015-02-08 14:44:22 +0000

    random number generator added to access random array element

--- c1/c1.cpp
+++ c1/c1.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <time.h>
+#include <cstdlib>
 using namespace std;
 
 int main() {
+
+    srand(time(NULL));
+    unsigned short randomNumber;
+    randomNumber = rand() % 5162 + 1;
 
     ifstream inputFile;
     inputFile.open("names.txt");
@@ -11,9 +17,12 @@ int main() {
     while (!inputFile.eof()) {
         for (int i = 0; i < 5162; i++) {
             getline(inputFile, names[i]);
-            cout << names[i] << endl;
         }
     }
+
+    cout << "Press enter for your random name!" << endl;
+    getchar();
+    cout << names[randomNumber] << endl;
 
 
     return 0;
