94583f8b5718146e6685d529f23af82f5b5b6141
brettalcox <brettalcox@gmail.com>
2015-02-08 14:37:31 +0000

    reading file into memory

--- c1/c1.cpp
+++ c1/c1.cpp
@@ -6,12 +6,14 @@ int main() {
 
     ifstream inputFile;
     inputFile.open("names.txt");
+    string names[5162];
 
-
-
-
-
-
+    while (!inputFile.eof()) {
+        for (int i = 0; i < 5162; i++) {
+            getline(inputFile, names[i]);
+            cout << names[i] << endl;
+        }
+    }
 
 
     return 0;
