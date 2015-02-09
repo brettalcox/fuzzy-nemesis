d09fea2fb10393ac24b8f3f30c1511ff47ecab47
brettalcox <brettalcox@gmail.com>
2015-02-09 17:01:24 +0000

    c6: features to deal with 'paper' and 'scissor'

--- c6/c6/c6.cpp
+++ c6/c6/c6.cpp
@@ -22,12 +22,46 @@ int main() {
         cin >> userChoice;
         userPlayer.setState(userChoice);
 
+        //User choosing Rock
+
         if ((userPlayer.getState() == 1) && (computerPlayer.getState() == 1)) {
-            cout << "Tie! Go again!" << endl;
+            cout << "Tie!" << endl;
         }
         else if ((userPlayer.getState() == 1) && (computerPlayer.getState() == 2)) {
-            cout << "Paper beats rock! Go again!" << endl;
+            cout << "Paper beats rock!" << endl;
             computerPlayer.addWin();
+        }
+        else if ((userPlayer.getState() == 1) && (computerPlayer.getState() == 3)) {
+            cout << "Rock beats scissors!" << endl;
+            userPlayer.addWin();
+        }
+
+        //User choosing Paper
+
+        if ((userPlayer.getState() == 2) && (computerPlayer.getState() == 1)) {
+            cout << "Paper beats rock!" << endl;
+            userPlayer.addWin();
+        }
+        else if ((userPlayer.getState() == 2) && (computerPlayer.getState() == 2)) {
+            cout << "Tie!" << endl;
+        }
+        else if ((userPlayer.getState() == 2) && (computerPlayer.getState() == 3)) {
+            cout << "Scissors beat paper!" << endl;
+            computerPlayer.addWin();
+        }
+
+        //User choosing scissors
+
+         if ((userPlayer.getState() == 3) && (computerPlayer.getState() == 1)) {
+            cout << "Rock beats scissor!" << endl;
+            computerPlayer.addWin();
+        }
+        else if ((userPlayer.getState() == 3) && (computerPlayer.getState() == 2)) {
+            cout << "Scissors beat paper!" << endl;
+            userPlayer.addWin();
+        }
+        else if ((userPlayer.getState() == 3) && (computerPlayer.getState() == 3)) {
+            cout << "Tie!" << endl;
         }
 
 
