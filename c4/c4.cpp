fcbd4fbc8146c4e570123bc24e5f49161e928d6f
brettalcox <brettalcox@gmail.com>
2015-02-09 00:49:04 +0000

    c4: input methods added

--- c4/c4.cpp
+++ c4/c4.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
+    string inputString;
+    int lengthOfString;
+    char * length;
+
+    cout << "Please enter a string..." << endl << endl << ": ";
+    getline(cin, inputString);
+    lengthOfString = inputString.length();
+    length = new char[lengthOfString];
+
+    for (int i = lengthOfString; i > 0; i--) {
+
+    }
+
+    cout << inputString;
 
     return 0;
 }
