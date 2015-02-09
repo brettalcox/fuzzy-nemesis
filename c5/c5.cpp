6f21a6236ef4e22d4add556dec3cfcd9cfcc6d98
brettalcox <brettalcox@gmail.com>
2015-02-09 10:53:33 +0000

    c5: added the fizzbuzz feature

--- c5/c5.cpp
+++ c5/c5.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main() {
 
     for (int i = 1; i < 101; i++) {
-        if (i % 3 == 0) {
+        if ((i % 3 == 0) && (i % 5 != 0)) {
             cout << "fizz, ";
         }
-        else if (i % 5 == 0) {
+        else if ((i % 5 == 0) && (i % 3 != 0)) {
             cout << "buzz, ";
+        }
+        else if ((i % 3 == 0) && (i % 5 == 0)) {
+            cout << "fizzbuzz, ";
         }
         else {
             cout << i << ", ";
