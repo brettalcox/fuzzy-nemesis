953e099d5f569bb237058d33769e0161222694f4
brettalcox <brettalcox@gmail.com>
2015-02-09 00:17:15 +0000

    c3: added the calculations to find seconds

--- c3/c3.cpp
+++ c3/c3.cpp
@@ -6,6 +6,11 @@ int main() {
     unsigned short years;
     unsigned short months;
     unsigned short days;
+    int seconds;
+
+    int yearsSeconds;
+    int monthsSeconds;
+    int daysSeconds;
 
     cout << "How many years old are you?: ";
     cin >> years;
@@ -13,6 +18,13 @@ int main() {
     cin >> months;
     cout << years << " years, " << months << " months, and how many days?: ";
     cin >> days;
+
+    yearsSeconds = ((((years * 365) * 24) * 60) * 60);
+    monthsSeconds = ((((months * 30) * 24) * 60) * 60);
+    daysSeconds = (((days * 24) * 60) * 60);
+    seconds = yearsSeconds + monthsSeconds + daysSeconds;
+
+    cout << "You are " << seconds << " seconds old!" << endl;
 
     return 0;
 }
