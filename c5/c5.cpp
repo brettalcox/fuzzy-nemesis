927828e6bbd0cb159f108a12fd3a288bf665aae4
brettalcox <brettalcox@gmail.com>
2015-02-09 10:49:08 +0000

    c5: added fizz and buzz but not fizzbuzz

--- c5/c5.cpp
+++ c5/c5.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 int main() {
 
+    for (int i = 1; i < 101; i++) {
+        if (i % 3 == 0) {
+            cout << "fizz, ";
+        }
+        else if (i % 5 == 0) {
+            cout << "buzz, ";
+        }
+        else {
+            cout << i << ", ";
+        }
+    }
     return 0;
 }
