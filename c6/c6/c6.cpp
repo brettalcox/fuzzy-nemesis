fa3d14904d4fc833d4de384ad05ea5765d9dbae4
brettalcox <brettalcox@gmail.com>
2015-02-09 16:12:48 +0000

    c6: added prompt for user to make choice

--- c6/c6/c6.cpp
+++ c6/c6/c6.cpp
@@ -8,7 +8,13 @@ int main() {
 
     srand(time(NULL));
     unsigned short hand = rand() % 3 + 1;
-    cout << "Welcome to Rock, Paper, Scissors!" << endl;
+    unsigned short userChoice;
+    cout << "Welcome to Rock, Paper, Scissors!" << endl << endl;
+
+    cout << "Rock, paper, or scissors?" << endl << endl;
+    cout << "1 = Rock, 2 = Paper, 3 = Scissors" << endl << endl << ": ";
+    cin >> userChoice;
+
 
     return 0;
 }
