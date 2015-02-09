87acebb797e83cbccc2224bc1626ee213b1386cd
brettalcox <brettalcox@gmail.com>
2015-02-09 01:10:46 +0000

    c4: have the general flow down. not quite working yet though

--- c4/c4.cpp
+++ c4/c4.cpp
@@ -5,18 +5,29 @@ using namespace std;
 int main() {
     string inputString;
     int lengthOfString;
-    char * length;
+    string * encryptArray;
+
+    int startSlice = 0;
+    int endSlice = 1;
 
     cout << "Please enter a string..." << endl << endl << ": ";
     getline(cin, inputString);
     lengthOfString = inputString.length();
-    length = new char[lengthOfString];
+    lengthOfString = (lengthOfString / 2) + 1;
+    encryptArray = new string[lengthOfString];
+    //string butt = "fart";
+    //encryptArray[0] = butt;
+    //cout << encryptArray[0];
 
-    for (int i = lengthOfString; i > 0; i--) {
-
+    for (int i = lengthOfString - 1; i > 0; i--) {
+        encryptArray[i] = inputString.substr(1, 2);
+        startSlice += 2;
+        endSlice += 2;
     }
 
-    cout << inputString;
+    for (int i = 0; i > lengthOfString; i++) {
+        cout << encryptArray[i];
+    }
 
     return 0;
 }
