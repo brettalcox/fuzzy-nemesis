6f6afaa5c1e14765b7d04c4791d8dd2109184c7b
brettalcox <brettalcox@gmail.com>
2015-02-11 01:02:28 +0000

    finds the number of each char in string and adds the outside to the middle

--- c9/c9.py
+++ c9/c9.py
@@ -8,11 +8,35 @@ def count_dict(loveString):
 		
 	for k in d:
 		print(k + ': ' + str(d[k]))
-		
-
+	return d
+	
 def main():
 	loveString = raw_input("Please type a string.\n")
-	count_dict(loveString)
+	d = {}
+	d = count_dict(loveString)
+	
+	
+	begin = 0;
+	end = -1;
+	addDict = {}
+	lengthOfD = len(d) 
+	if (lengthOfD % 2 == 0):
+		for i in range(0, lengthOfD / 2):
+			addDict[i] = d[d.keys()[begin]] + d[d.keys()[end]]
+			begin += 1 
+			end -= 1
+	else:
+		for i in range(0, (lengthOfD / 2) + 1):
+			if (begin == (len(d) / 2)):
+				addDict[i] = d[d.keys()[begin]]
+			else:
+				addDict[i] = d[d.keys()[begin]] + d[d.keys()[end]]
+				begin += 1 
+				end -= 1
+				
+			
+	print (addDict)
+	
 	return 0
 
 if __name__ == '__main__':
