a074a52479bac156ec777fa9c5242aa42e9538ab
brettalcox <brettalcox@gmail.com>
2015-02-18 18:37:38 -0600

    12: program reads ntp time back and subtracts system time to find difference

--- 12/main.cpp
+++ 12/main.cpp
@@ -8,8 +8,11 @@
 #include <string.h>
 #include <iostream>
 
+void ntpdate();
+
 int main() {
 
+    ntpdate();
     return 0;
 }
 
@@ -22,5 +25,28 @@ void ntpdate() {
     unsigned char msg[48] = {010, 0, 0, 0, 0, 0, 0, 0, 0,};
     unsigned long buf[maxlen];
 
+    struct protoent *proto;
+    struct sockaddr_in server_addr;
+    int s;
+    long tmit;
 
+    proto = getprotobyname("udp");
+    s = socket(PF_INET, SOCK_DGRAM, proto->p_proto);
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(hostname);
+    server_addr.sin_port = htons(portno);
+
+    i = sendto(s, msg, sizeof(msg), 0, (struct sockaddr *) &server_addr, sizeof (server_addr));
+    struct sockaddr saddr;
+    socklen_t saddr_l = sizeof (saddr);
+    i = recvfrom(s, buf, 48, 0, &saddr, &saddr_l);
+
+    tmit = ntohl((time_t)buf[4]);
+
+    tmit -= 2208988800U;
+    std::cout << "NTP time is " << ctime(&tmit) << std::endl;
+    i = time(0);
+    std::cout << "System time is " << (i - tmit) << " seconds off" << std::endl;
 }
